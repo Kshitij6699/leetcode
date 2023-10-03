08671837afe378183b33cfeffaf76e156d3ef418
Kshitij <mankarkshitij619@gmail.com>
2023-10-03 19:42:04 +0530

    1. PivotElement<->2. sorted half

--- Leetcode_python/Search and Sorting/SearchElementInRotatedArray(unique).py
+++ Leetcode_python/Search and Sorting/SearchElementInRotatedArray(unique).py
@@ -33,7 +33,7 @@ class Solution:
                 s = mid + 1
         return - 1
 
-    def search(self, nums: List[int], target: int) -> int:
+    def search(self, nums, target):
         s = 0
         e = len(nums) - 1
         ans = -1
